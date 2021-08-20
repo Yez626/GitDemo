57d3c7a3cc436a6e0c633fb480225946f70a95a3
Yuan126 <67539898+Yuan126@users.noreply.github.com>
2021-08-20 11:34:47 +0800

    https://github.com/Yez626/GitDemo.git

---
+++ Java/oooo/DogLauncher.java
@@ -0,0 +1,13 @@
+package oooo;
+
+public class DogLauncher {
+    public static void main(String[] args) {
+    Dog smallDog;                     //declaration of a dog variable//
+    new Dog(20);                      //instantiation of the dog class as a dog object//
+               smallDog = new Dog(5); //instantiation and assignment//    
+           Dog hugeDog = new Dog(150);//declaration,instantiation and assignment//
+               smallDog.makeNoise();
+           hugeDog.makeNoise();
+    }
+    }
+    
